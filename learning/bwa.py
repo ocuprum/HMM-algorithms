07129b2d80fae8ac7f1b41ec12b5d12db9946e19
Oleksandra Mednikova <olecuprum@gmail.com>
2022-10-27 11:32:25 +0300

    BWA: added scaling

--- learning/bwa.py
+++ learning/bwa.py
@@ -1,28 +1,37 @@
 import numpy as np
-from estimation.fba import FBA
+from evaluation.fba import FBA
 
 class BWA(FBA):
     '''Baum-Welch algorithm'''
     def __init__(self, distribution, transition, output, observation):
         FBA.__init__(self, distribution, transition, output, observation)
 
-    def __transit(self, prob):
+    def __transit(self):
         self.i_transit = np.matrix(np.zeros((self.t, self.s)))
         self.ij_transit = np.zeros((self.t - 1, self.s, self.s))
 
         for time in range(self.t):
-            if time != self.t - 1: y = self.observation[time+1]
+            if time != self.t - 1: 
+                y = self.observation[time+1]
+
+                prob_i, prob_ij = 0, 0
+                for i in range(self.s):
+                    prob_i += self.alpha[time, i] * self.beta[time, i]
+                    for j in range(self.s):
+                        prob_ij += self.alpha[time, i] * self.transition[i, j] \
+                            * self.output[j, y] * self.beta[time+1, j]
+
             for i in range(self.s):
-                self.i_transit[time, i] = self.alpha[time, i] * self.beta[time, i] / prob
+                self.i_transit[time, i] = self.alpha[time, i] * self.beta[time, i] / prob_i
                 for j in range(self.s):
                     if time != self.t - 1:
                         self.ij_transit[time, i, j] = self.alpha[time, i] * self.transition[i, j] \
-                                                * self.output[j, y] * self.beta[time+1, j] / prob
+                                                * self.output[j, y] * self.beta[time+1, j] / prob_ij
 
     def __step(self):
-        prob = self.forward()
-        self.backward()
-        self.__transit(prob)
+        self.forward()
+        prob = self.backward()
+        self.__transit()
 
         self.distribution = np.array(self.i_transit[0])[0]
         for i in range(self.s):
@@ -34,12 +43,16 @@ class BWA(FBA):
                 k_sum = sum([self.i_transit[t, i] for t in range(self.t) if self.observation[t] == k])
                 i_sum = sum([self.i_transit[t, i] for t in range(self.t)])
                 self.output[i, k] = k_sum / i_sum
+        
+        return prob
     
-    def learn(self, epsilon=0.001):
-        prev_prob = self.forward()
-        self.__step()
-        cur_prob = self.forward()
-        while cur_prob - prev_prob > epsilon:
+    def learn(self, iters=1000, epsilon=0.0000001):
+        prev_prob = 0 
+
+        for i in range(iters):
+            cur_prob = self.__step() 
+            if abs(cur_prob - prev_prob) < epsilon: break
             prev_prob = cur_prob
-            self.__step()
-            cur_prob = self.forward()
+
+            if i % 10 == 0: print('{} - {}'.format(i, cur_prob))
+            
